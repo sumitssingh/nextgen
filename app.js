2f6ff4855e9a6db900cd3b21270a376002fadeb5
sumit-ssingh <sumitssingh943@gmail.com>
2017-12-09 23:41:33 +0530

    added function to create data

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 
-   
+   // var db = connect('heroku_95qwwgj8:6iuv3jlc5vkoqbfuf651agu0jv@ds023490.mlab.com:23490/heroku_95qwwgj8')
+   var _ = require('underscore');
+   var mongoose = require('mongoose');
+    
+    var db = connect('mongodb://107.170.218.205:27017/panoProd');
     var sql = require("mssql");;
     var config = {
         user: 'svc_oncall',
@@ -13,9 +17,16 @@
         var request = new sql.Request();
            console.log("connected");
         request.query("select * from viewDrCardCategories where working_date='20091023' union select * from viewDrCardClinicLocations where working_date='20091023' union select * from viewDrCardAppointments where working_date='20091023' order by description, begintime", function (err, recordset) {
-            
-            if (err) console.log(err)
-            res.send(recordset);
+            if (err)  {
+                console.log(err)
+            }
+            _.forEach(recordset.recordset, function(data) {
+                var appointment  = [];
+                var appointmenTime = data.working_date + ' ' + data.begintime;
+                appointment.push({'appointmenTime': appointmenTime, 'location': data.Location})
+                db.collection('Docter').insert({username:data.description,Appointment:appointmenTime})
+            });
+
             
         });
     });
