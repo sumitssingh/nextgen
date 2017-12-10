b7504f575a60a83323eba5aa2d8ceafa81b0fa67
sumit-ssingh <sumitssingh943@gmail.com>
2017-12-10 11:22:18 +0530

    added script to run mongo

--- app.js
+++ app.js
@@ -1,60 +1,16 @@
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
 
-   // // var db = connect('heroku_95qwwgj8:6iuv3jlc5vkoqbfuf651agu0jv@ds023490.mlab.com:23490/heroku_95qwwgj8')
-   // var _ = require('underscore');
-   // // var mongoose = require('mongoose');
-    
-   //  // var db = connect('mongodb://107.170.218.205:27017/panoProd');
-   //  var MongoClient = require('mongodb').MongoClient;
-   //  var url = "mongodb://107.170.218.205:27017/mydb";
+// Connection URL
+var url = 'mongodb://localhost:27017/myproject';
 
-   //  MongoClient.connect(url, function(err, db) {
-   //    if (err) throw err;
-   //    console.log("Database created!");
-   //    db.close();
-   //  });
-   //  var sql = require("mssql");;
-   //  var config = {
-   //      user: 'svc_oncall',
-   //      password: 'gOGJhG6K1w',
-   //      server: 'xposc-nextgen01', 
-   //      database: 'NGProd'
-   //  };
-   //  sql.connect(config, function (err) {
-    
-   //      if (err) console.log('err '+err);
-   //      var request = new sql.Request();
-   //         console.log("connected");
-   //      request.query("select * from viewDrCardCategories where working_date='20091023' union select * from viewDrCardClinicLocations where working_date='20091023' union select * from viewDrCardAppointments where working_date='20091023' order by description, begintime", function (err, recordset) {
-   //          if (err)  {
-   //              console.log(err)
-   //          }
-   //          _.forEach(recordset.recordset, function(data) {
-   //              var appointment  = [];
-   //              var appointmenTime = data.working_date + ' ' + data.begintime;
-   //              appointment.push({'appointmenTime': appointmenTime, 'location': data.Location})
-   //              db.collection('Docter').insert({username:data.description,Appointment:appointmenTime})
-   //          });
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
 
-            
-   //      });
-   //  });
-
-    var express = require('express');
-var app = express();
- var _ = require('underscore');
-   // var mongoose = require('mongoose');
-    
-    // var db = connect('mongodb://107.170.218.205:27017/panoProd');
-    var MongoClient = require('mongodb').MongoClient;
-    var url = "mongodb://107.170.218.205:27017/mydb";
-
-    MongoClient.connect(url, function(err, db) {
-      if (err) throw err;
-      console.log("Database created!");
-      db.close();
-    });
-app.get('/', function (req, res) {
-   
+  db.close();
+});
     var sql = require("mssql");;
     var config = {
         user: 'svc_oncall',
@@ -71,18 +27,22 @@ app.get('/', function (req, res) {
             if (err)  {
                 console.log(err)
             }
-            _.forEach(recordset, function(data) {
+            _.forEach(recordset.recordset, function(data) {
                 var appointment  = [];
-                var appointmenTime = data.recordset.working_date + ' ' + data.recordset.begintime;
-                appointment.push({'appointmenTime': appointmenTime, 'location': data.recordset.Location})
-                db.collection('Docter').insert({username:data.recordset.description,Appointment:appointmenTime})
+                var appointmenTime = data.working_date + ' ' + data.begintime;
+                appointment.push({'appointmenTime': appointmenTime, 'location': data.Location})
+                var insertDocuments = function(db, callback) {
+                    var collection = db.collection('doctors');
+                        collection.insert({username:data.description,Appointment:appointmenTime}, function(err, result) {
+                            if (err) {
+                                console.log(err)
+                            }else {
+                                console.log(success);
+                            }
+                        })
+                    }    
             });
 
             
         });
     });
-});
-
-var server = app.listen(5000, function () {
-    console.log('Server is running..');
-});
