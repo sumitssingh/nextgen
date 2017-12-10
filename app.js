203f08506730710efeaa9c7d30b82697f1c1f65d
sumit-ssingh <sumitssingh943@gmail.com>
2017-12-10 11:52:10 +0530

    link fix

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 var MongoClient = require('mongodb').MongoClient
   , assert = require('assert');
+  var _ = require('underscore');
+var url = 'mongodb://localhost:27017/panoProd';
 
-// Connection URL
-var url = 'mongodb://localhost:27017/myproject';
-
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  db.close();
-});
+// MongoClient.connect(url, function(err, db) {
+//   assert.equal(null, err);
+//   console.log("Connected successfully to server");
+//   insertDocuments(db, function() {
+//   db.close();
+// });
+// });  
     var sql = require("mssql");;
     var config = {
         user: 'svc_oncall',
@@ -26,21 +25,29 @@ MongoClient.connect(url, function(err, db) {
         request.query("select * from viewDrCardCategories where working_date='20091023' union select * from viewDrCardClinicLocations where working_date='20091023' union select * from viewDrCardAppointments where working_date='20091023' order by description, begintime", function (err, recordset) {
             if (err)  {
                 console.log(err)
-            }
+            }MongoClient.connect(url, function(err, db) {
+              assert.equal(null, err);
+              console.log("Connected successfully to server");
+              // insertDocuments(db, function() {
+              // db.close();
+            // });
+            // });  
             _.forEach(recordset.recordset, function(data) {
                 var appointment  = [];
                 var appointmenTime = data.working_date + ' ' + data.begintime;
                 appointment.push({'appointmenTime': appointmenTime, 'location': data.Location})
-                var insertDocuments = function(db, callback) {
+                // var insertDocuments = function(db, callback) {
                     var collection = db.collection('doctors');
-                        collection.insert({username:data.description,Appointment:appointmenTime}, function(err, result) {
+                        // collection.insert({username:data.description,Appointment:appointmenTime}, function(err, result) {
+                        collection.insert({username:'sumit'}, function(err, result) {
                             if (err) {
                                 console.log(err)
                             }else {
-                                console.log(success);
+                                console.log(docs);
                             }
                         })
-                    }    
+                    })  
+            db.close();
             });
 
             
