56a39dce69fdbabbeb4f8a84b060c3bd5d566e1a
sumit-ssingh <sumitssingh943@gmail.com>
2017-12-10 09:36:37 +0530

    added mongodb

--- app.js
+++ app.js
@@ -1,6 +1,47 @@
 
-   // var db = connect('heroku_95qwwgj8:6iuv3jlc5vkoqbfuf651agu0jv@ds023490.mlab.com:23490/heroku_95qwwgj8')
-   var _ = require('underscore');
+   // // var db = connect('heroku_95qwwgj8:6iuv3jlc5vkoqbfuf651agu0jv@ds023490.mlab.com:23490/heroku_95qwwgj8')
+   // var _ = require('underscore');
+   // // var mongoose = require('mongoose');
+    
+   //  // var db = connect('mongodb://107.170.218.205:27017/panoProd');
+   //  var MongoClient = require('mongodb').MongoClient;
+   //  var url = "mongodb://107.170.218.205:27017/mydb";
+
+   //  MongoClient.connect(url, function(err, db) {
+   //    if (err) throw err;
+   //    console.log("Database created!");
+   //    db.close();
+   //  });
+   //  var sql = require("mssql");;
+   //  var config = {
+   //      user: 'svc_oncall',
+   //      password: 'gOGJhG6K1w',
+   //      server: 'xposc-nextgen01', 
+   //      database: 'NGProd'
+   //  };
+   //  sql.connect(config, function (err) {
+    
+   //      if (err) console.log('err '+err);
+   //      var request = new sql.Request();
+   //         console.log("connected");
+   //      request.query("select * from viewDrCardCategories where working_date='20091023' union select * from viewDrCardClinicLocations where working_date='20091023' union select * from viewDrCardAppointments where working_date='20091023' order by description, begintime", function (err, recordset) {
+   //          if (err)  {
+   //              console.log(err)
+   //          }
+   //          _.forEach(recordset.recordset, function(data) {
+   //              var appointment  = [];
+   //              var appointmenTime = data.working_date + ' ' + data.begintime;
+   //              appointment.push({'appointmenTime': appointmenTime, 'location': data.Location})
+   //              db.collection('Docter').insert({username:data.description,Appointment:appointmenTime})
+   //          });
+
+            
+   //      });
+   //  });
+
+    var express = require('express');
+var app = express();
+ var _ = require('underscore');
    // var mongoose = require('mongoose');
     
     // var db = connect('mongodb://107.170.218.205:27017/panoProd');
@@ -12,6 +53,8 @@
       console.log("Database created!");
       db.close();
     });
+app.get('/', function (req, res) {
+   
     var sql = require("mssql");;
     var config = {
         user: 'svc_oncall',
@@ -28,13 +71,18 @@
             if (err)  {
                 console.log(err)
             }
-            _.forEach(recordset.recordset, function(data) {
+            _.forEach(recordset, function(data) {
                 var appointment  = [];
-                var appointmenTime = data.working_date + ' ' + data.begintime;
-                appointment.push({'appointmenTime': appointmenTime, 'location': data.Location})
-                db.collection('Docter').insert({username:data.description,Appointment:appointmenTime})
+                var appointmenTime = data.recordset.working_date + ' ' + data.recordset.begintime;
+                appointment.push({'appointmenTime': appointmenTime, 'location': data.recordset.Location})
+                db.collection('Docter').insert({username:data.recordset.description,Appointment:appointmenTime})
             });
 
             
         });
     });
+});
+
+var server = app.listen(5000, function () {
+    console.log('Server is running..');
+});
