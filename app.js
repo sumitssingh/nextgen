dc55a89fd746bfaa6e721dcf348983eab883c58f
sumit-ssingh <sumitssingh943@gmail.com>
2017-12-11 23:48:48 +0530

    dsrsdea

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ var url = 'mongodb://107.170.218.205:27017/panoProd3';
                 }];
                 var data = recordset;
                 console.log(data.recordset[0]);
-                console.log(newData[0].Appointment);
+                console.log(newData[0]);
                 for (var i =0; i<data.recordset.length;i++) {
                     newData.username = data.recordset[i].description;
                     newData[0].Appointment.push({'appointmenTime': data.recordset[i].working_date + ' ' + data.recordset[i].begintime, 'location': data.recordset[i].Location})
